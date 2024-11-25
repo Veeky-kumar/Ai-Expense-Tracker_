d0dd737517e6a76fc9890ac6a01086f2448c5d1a
Veeky <vk133162@gamil.com>
2024-11-25 07:18:50 +0530

    added default route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-// import Layout from "./components/layout/Layout"
-import Dashboard from "./pages/Dashboard"
-import Layout from "./components/Layout/Layout"
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./components/Layout/Layout";
 
 function App() {
-
   return (
     <>
-   <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />} >
-        <Route path='/api/dashboard' element={<Dashboard/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* Default route */}
+            <Route index element={<Dashboard />} />
+            {/* Explicit dashboard route */}
+            <Route path="api/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
