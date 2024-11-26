0ef9e447210602c1f3be4f7d5ddb4fa44b089246
Veeky <vk133162@gamil.com>
2024-11-27 01:18:55 +0530

    final deploy3

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -6,7 +6,7 @@ const createExpense = async (req, res) => {
         const {name,category, amount } = req.body;
         const newExpense = new Expense({ name, category, amount });
         await newExpense.save();
-        console.log("inside controller", newExpense);
+        // console.log("inside controller", newExpense);
         res.status(201).json({ expense: newExpense, message: "Expense created successfully" });
     } catch (error) {
         res.status(404).json({ error: error, message:" Error"});
@@ -16,7 +16,7 @@ const createExpense = async (req, res) => {
 const getExpenseList = async (req, res) => {
     try {
         const allExpense = await Expense.find({ });
-        console.log("inside controller", allExpense);
+        // console.log("inside controller", allExpense);
         res.status(201).json({ expense: allExpense, message: "Expense fetched successfully" });
     } catch (error) {
         res.status(404).json({ error: error, message:" Error"});
