81b55a8aa46e7cc614fa9609eb5e0a2a8dacb90c
Veeky <vk133162@gamil.com>
2024-11-27 06:10:57 +0530

    open ai final done u

--- src/api/expenseApi.js
+++ src/api/expenseApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_BASE_URL = "https://ai-expense-tracker-ckpm.onrender.com/api"; 
-const API_BASE_URL = "http://localhost:5000/api"; 
+const API_BASE_URL = "https://ai-expense-tracker-ckpm.onrender.com/api"; 
+// const API_BASE_URL = "http://localhost:5000/api"; 
 
 const createExpense = async (formData) => {
   try {
